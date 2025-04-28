50daeb29ec762aeb512d32a244b86ecd27414c7e
royxiong <sx24@illinois.edu>
2025-04-28 15:17:27 +0000

    example cycle detection

--- includes/graph.hpp
+++ includes/graph.hpp
@@ -3,22 +3,31 @@
 
 #include <list>
 #include <map>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
 
 class Graph {
-    public:
-        Graph() = default;
+public:
+  Graph() = default;
 
-        void addVertex(std::string);
+  void addVertex(std::string);
 
-        void addEdge(std::string source, std::string destination);
+  void addEdge(std::string source, std::string destination);
 
-        void getEdges(std::string source);
+  void getEdges(std::string source);
 
-        void areAdjacent(std::string source, std::string destination);
+  void areAdjacent(std::string source, std::string destination);
 
-    private:
-        std::list<std::string> nodes_;
-        std::map<std::string, std::list<std::string>> edges_;
+  bool dfs(const std::string& node,
+           std::unordered_set<std::string>& visited,
+           std::unordered_set<std::string>& recursionStack);
+
+  bool detectCycle();
+
+private:
+  std::list<std::string> nodes_;
+  std::map<std::string, std::list<std::string>> edges_;
 };
 
 #endif GRAPH_HPP
